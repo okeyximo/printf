cea1778046b50958a7529aef49923e54b899566a
Veralee225 <Veralee225@gmail.com>
2022-04-19 00:49:26 +0100

    updated the header file

--- main.h
+++ main.h
@@ -1,5 +1,18 @@
-#ifndef HEADER_MAIN
-#define HEADER_MAIN
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdarg.h>
+#include <assert.h>
+#include <stdbool.h>
+
+/* macros */
+#define FLUSH -1
+#define PF_INIT {false, false, false}
+#define NIL "(nil)"
+#define HEXA "0x"
+#define NULL_STRING "(null)"
+#define NUL '\0'
+#define KILOBYTE 1024
+#define ABS(x) (((x) < 0) ? -(x) : (x))
 
 #endif
