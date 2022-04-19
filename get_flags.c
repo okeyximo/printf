3960c4d7b60c6792d319c333be3dbf95b19d9612
Veralee225 <Veralee225@gmail.com>
2022-04-19 08:13:12 +0100

    fixed some errors

--- get_flags.c
+++ get_flags.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * get flags - turns on flags if _printf finds a
- * flag modifier in the format string
- * @s: character that holds the flag spacifier
+ * get_flags - turns on flags if _printf finds
+ * a flag modifier in the format string
+ * @s: character that holds the flag specifier
  * @f: pointer to the struct flags in which we turn the flags on
- * 
- * Return: 1 if a flag has been turned on, 0 otherwise          
+ *
+ * Return: 1 if a flag has been turned on, 0 otherwise
  */
-
 int get_flags(char s, mods *f)
 {
-    bool modifier = true;
+	_Bool modifier = true;
 
-    switch (s)
-    {
-        case '+':
-                f->plus = true;
-                break;
-        case '':
-                f->space = true;
-                break;
-        case '#':
-    
-    }
+	switch (s)
+	{
+		case '+':
+			f->plus = true;
+			break;
+		case ' ':
+			f->space = true;
+			break;
+		case '#':
+			f->hash = true;
+			break;
+		default:
+			modifier = false;
+	}
+	return (modifier);
 }
