6cec2248c8dcab74c167448ab5cc4ec589480c32
Veralee225 <Veralee225@gmail.com>
2022-04-19 09:08:09 +0100

    invalid input function

---
+++ assertions.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * invalidInputs - validates user did not enter invalid inputs
+ * @p: inputs string
+ * Return: true or false
+ */
+bool invalidInputs(const char *p)
+{
+    /* NULL */
+    if (!p)
+            return (false);
+    /* "%" */
+    if (*p == '%' && !*(p + 1))
+            return (false);
+    /* "% " */
+    if (*p == '%' && *(p + 1) == ' ' && !*(p + 2))
+            return (false);
+    retrun (true);
+}
