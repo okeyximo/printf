f745bf1ad1d8d734ed8646d57634f67ccd7d3db0
okeyximo <xcellnximo57@gmail.com>
2022-04-19 21:45:47 +0000

    fixed betty

--- get_print.c
+++ get_print.c
@@ -12,28 +12,28 @@
  */
 int (*get_print(char s))(va_list, mods *)
 {
-    ph func_arr[] = {
-            {'i', print_int},
-            {'s', print_string},
-            {'c', print_char},
-            {'d', print_int},
-            {'u', print_unsigned},
-            {'x', print_hex},
-            {'X', print_hex_big},
-            {'b', print_binary},
-            {'o', print_octal},
-            {'R', print_rot13},
-            {'r', print_rev},
-            {'S', print_bigS},
-            {'p', print_address},
-            {'%', print_percent},
-            {NUL, NULL}
-            };
-    
-    register short i;
+	ph func_arr[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent},
+		{NUL, NULL}
+	};
 
-    for (i = 0; func_arr[i].c; i++)
-            if (func_arr[i].c == s)
-                    return (func_arr[i].f);
-    return(NULL);
+	register short i;
+
+	for (i = 0; func_arr[i].c; i++)
+		if (func_arr[i].c == s)
+			return (func_arr[i].f);
+	return (NULL);
 }
